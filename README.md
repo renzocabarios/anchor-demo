61975bdb39868a864850562722f35b6b2e6578cd
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-22 03:05:10 +0800

    Update hello_world

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 -`cargo stylus deploy --private-key-path=<PRIVATE_KEY> --estimate-gas-only` get gas estimation
 -`cargo stylus deploy --private-key-path=<PRIVATE_KEY>`
 -`cargo stylus export-abi`
+- Where you can find YOUR_ARCHITECTURE by running `rustc -vV | grep host`
+- `cargo run --example counter --target=<YOUR_ARCHITECTURE>`
+
--- hello_world/examples/counter.rs
+++ hello_world/examples/counter.rs
@@ -17,22 +17,22 @@ use std::str::FromStr;
 use std::sync::Arc;
 
 /// Your private key file path.
-const ENV_PRIV_KEY_PATH: &str = "PRIV_KEY_PATH";
+const ENV_PRIV_KEY_PATH: &str = "../wallet.txt";
 
 /// Stylus RPC endpoint url.
-const ENV_RPC_URL: &str = "RPC_URL";
+const ENV_RPC_URL: &str = "https://stylus-testnet.arbitrum.io/rpc";
 
 /// Deployed pragram address.
-const ENV_PROGRAM_ADDRESS: &str = "STYLUS_PROGRAM_ADDRESS";
+const ENV_PROGRAM_ADDRESS: &str = "0x05911DE732cb96CdA51C9a411072984D163EcAcf";
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let priv_key_path = std::env::var(ENV_PRIV_KEY_PATH)
-        .map_err(|_| eyre!("No {} env var set", ENV_PRIV_KEY_PATH))?;
-    let rpc_url =
-        std::env::var(ENV_RPC_URL).map_err(|_| eyre!("No {} env var set", ENV_RPC_URL))?;
-    let program_address = std::env::var(ENV_PROGRAM_ADDRESS)
-        .map_err(|_| eyre!("No {} env var set", ENV_PROGRAM_ADDRESS))?;
+    // let priv_key_path = std::env::var(ENV_PRIV_KEY_PATH)
+    //     .map_err(|_| eyre!("No {} env var set", ENV_PRIV_KEY_PATH))?;
+    // let rpc_url =
+    //     std::env::var(ENV_RPC_URL).map_err(|_| eyre!("No {} env var set", ENV_RPC_URL))?;
+    // let program_address = std::env::var(ENV_PROGRAM_ADDRESS)
+    //     .map_err(|_| eyre!("No {} env var set", ENV_PROGRAM_ADDRESS))?;
     abigen!(
         Counter,
         r#"[
@@ -42,10 +42,10 @@ async fn main() -> eyre::Result<()> {
         ]"#
     );
 
-    let provider = Provider::<Http>::try_from(rpc_url)?;
-    let address: Address = program_address.parse()?;
+    let provider = Provider::<Http>::try_from("https://stylus-testnet.arbitrum.io/rpc")?;
+    let address: Address = "0x05911DE732cb96CdA51C9a411072984D163EcAcf".parse()?;
 
-    let privkey = read_secret_from_file(&priv_key_path)?;
+    let privkey = "e36d87e4ed845992fcdf3028d37f1cd062118cd8f1722f2855bf5d42cc50fa86";
     let wallet = LocalWallet::from_str(&privkey)?;
     let chain_id = provider.get_chainid().await?.as_u64();
     let client = Arc::new(SignerMiddleware::new(
@@ -53,7 +53,7 @@ async fn main() -> eyre::Result<()> {
         wallet.clone().with_chain_id(chain_id),
     ));
 
-    let counter = Counter::new(address, client);
+    let counter: Counter<SignerMiddleware<Provider<Http>, _>> = Counter::new(address, client);
     let num = counter.number().call().await;
     println!("Counter number value = {:?}", num);
 
